a7c8a98cabe673f7f7ffdcf1ccea71f0931d37da
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-07-09 10:00:55 -0400

    01 introduction

---
+++ 01_utilityPostgress/01_utilityPostgress.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+# La cosa de arriba le indica al terminal que es un comando
+
+# Esto es un comentario. Asi se comenta
+
+# Este programa es para realizar algunas operaciones utilitarias de Postgres
+
+echo "Hola bienvenido :3"
