97d62698629bef4c512436e366fb7e3fd5fedd38
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-07-21 14:06:21 -0400

    refactor: add functions

--- retos/menu.sh
+++ retos/menu.sh
@@ -4,7 +4,7 @@
 
 option=0
 
-while :; do
+introduction_menu() {
   clear
   cowsay "Hola humano. Bienvenido. Por favor, ¿Qué se le ofrece?"
   echo -e "\nMenu de opciones:"
@@ -18,59 +18,78 @@ while :; do
   echo "8. Salir"
   read -p "Ingrese una opción: " option
   echo -e "\n"
+}
+
+showProcess() {
+  cowsay "Ha elegido la opción $option. Veamos los procesos actuales."
+  echo -e "\nProcesos actuales:"
+  ps
+  sleep 3
+}
+
+showMemoryFree() {
+  cowsay "Ha elegido la opción $option. Veamos la memoria disponible."
+  echo -e "\nMemoria disponible:"
+  free -h
+  sleep 3
+}
+
+showDiskFree() {
+  cowsay "Ha elegido la opción $option. Veamos el espacio en el disco."
+  echo -e "\nEspacio en el disco:"
+  df -h / /home
+  sleep 3
+}
+
+showNetworkInformation(){
+  cowsay "Ha elegido la opción $option. Veamos la información de red."
+  echo -e "\nInformación de red:"
+  ifconfig
+  sleep 3
+}
+
+showEnviromentsVar() {
+  cowsay "Ha elegido la opción $option. Veamos las variables de entorno configuradas."
+  echo -e "\nEspere un momento..."
+  sleep 2
+  printenv | less
+  #env -u VAR
+  sleep 3
+}
+
+showProgramSystem() {
+  cowsay "Ha elegido la opción $option. Veamos los programas del sistema."
+  echo -e "\nEspere un momento..."
+  sleep 2
+  dpkg -l | less
+  sleep 3
+}
+
+doBackup() {
+  cowsay "Ha elegido la opción $option. ¿Hacemos un Backup?"
+  sleep 3
+  cowsay "Pos, ahora no esta disponible."
+  sleep 2
+}
+
+exitMenu() {
+  cowsay "Saliendo del programa... hasta la proxima"
+  sleep 2
+  clear
+  exit
+}
+
+while :; do
+  introduction_menu
 
   case $option in
-    1 )
-      cowsay "Ha elegido la opción $option. Veamos los procesos actuales."
-      echo -e "\nProcesos actuales:"
-      ps
-      sleep 3
-      ;;
-    2 )
-      cowsay "Ha elegido la opción $option. Veamos la memoria disponible."
-      echo -e "\nMemoria disponible:"
-      free -h
-      sleep 3
-      ;;
-    3 )
-      cowsay "Ha elegido la opción $option. Veamos el espacio en el disco."
-      echo -e "\nEspacio en el disco:"
-      free -h
-      sleep 3
-      ;;
-    4 )
-      cowsay "Ha elegido la opción $option. Veamos la información de red."
-      echo -e "\nInformación de red:"
-      ifconfig wlp3s0
-      sleep 3
-      ;;
-    5 )
-      cowsay "Ha elegido la opción $option. Veamos las variables de entorno configuradas."
-      echo -e "\nEspere un momento..."
-      sleep 2
-      printenv | less
-      #env -u VAR
-      sleep 3
-      ;;
-    6 )
-      cowsay "Ha elegido la opción $option. Veamos los programas del sistema."
-      echo -e "\nEspere un momento..."
-      sleep 2
-      dpkg -l | less
-      sleep 3
-      ;;
-    7 )
-      cowsay "Ha elegido la opción $option. ¿Hacemos un Backup?"
-      sleep 3
-      cowsay "Pos, ahora no esta disponible."
-      sleep 2
-      ;;
-    8 )
-      cowsay "Saliendo del programa... hasta la proxima"
-      sleep 2
-      exit
-      ;;
+    1 ) showProcess ;;
+    2 ) showMemoryFree ;;
+    3 ) showDiskFree ;;
+    4 ) showNetworkInformation ;;
+    5 ) showEnviromentsVar ;;
+    6 ) showProgramSystem ;;
+    7 ) doBackup ;;
+    8 ) exitMenu ;;
   esac
 done
-
-clear
